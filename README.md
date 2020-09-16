d8040539db4c462e558d84a425e2ea145cf60931
Edward NoaLand <771818272@qq.com>
2020-09-16 13:36:20 +0800

    feat & doc: add NullAllocator and update doc on todo

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Implement my own rust-version malloc
 
 Will implement it sooner or later.
 
+## What I gonna do
+Follow the link [Allocation API, allocators and virtual memory](https://notes.iveselov.info/programming/allocation-api-and-allocators) to understand the logic inside the allocator in rust,
+and then I will try to follow the other link to implement my own malloc & allocator
+
 ## Reference
 I get inspiration from the following link:
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::alloc::{GlobalAlloc, Layout};
+
+struct NullAllocator;
+
+unsafe impl GlobalAlloc for NullAllocator {
+    unsafe fn alloc(&self, _layout: Layout) -> *mut u8 {
+        std::ptr::null_mut()
+    }
+
+    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
+        panic!("won't deallocate: we never allocate!");
+    }
+}
+
+#[global_allocator]
+static A: NullAllocator = NullAllocator;
+
 fn main() {
-    println!("Hello, world!");
 }
