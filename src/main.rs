b4aa3038bf91a4b10b283396b30d3b77f8c02247
Edward NoaLand <771818272@qq.com>
2020-09-18 13:28:55 +0800

    feat: add stderr, stdout, eprintln and println alloc bytes compare

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io::Write;
 use std::alloc::{GlobalAlloc, Layout, System};
 use std::sync::atomic::{AtomicUsize, Ordering::SeqCst};
 
@@ -5,11 +6,20 @@ static ALLOCATED: AtomicUsize = AtomicUsize::new(0);
 
 struct Counter;
 
+fn print(args: std::fmt::Arguments<'_>) {
+    std::io::stderr().write_fmt(args).unwrap()
+}
+
 unsafe impl GlobalAlloc for Counter {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
         let ret = System.alloc(layout);
         if !ret.is_null() {
             ALLOCATED.fetch_add(layout.size(), SeqCst);
+            print(format_args!(
+                "allocated {} bytes with align {}\n",
+                layout.size(),
+                layout.align(),
+            ));
         }
         return ret;
     }
@@ -24,5 +34,20 @@ unsafe impl GlobalAlloc for Counter {
 static A: Counter = Counter;
 
 fn main() {
-    println!("allocated before main: {:?}", ALLOCATED.load(SeqCst));
+    let before_main = ALLOCATED.load(SeqCst);
+    print(format_args!(
+        "allocated bytes before main: {}\n",
+        before_main,
+    ));
+
+    // std::io::stdout().write(b"hello").unwrap(); // -- assign 1256 bytes
+    std::io::stderr().write(b"hello").unwrap(); // -- assign 0 bytes
+    // std::io::stderr().write_fmt(format_args!("{}", 1)).unwrap(); // -- assign 0 bytes
+    // eprintln!("hello"); // -- assign 0 bytes
+    // println!("hello"); // -- assign 1256 bytes
+
+    print(format_args!(
+        "allocated bytes for the construct: {}\n",
+        ALLOCATED.load(SeqCst) - before_main,
+    ));
 }
